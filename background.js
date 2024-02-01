015e11dc20bec9e16ac732379d41f43f3b3308dc
Tudor Ioan Mihail <113465589+tudormihail5@users.noreply.github.com>
2024-02-01 22:08:12 +0000

    Update background.js

--- background.js
+++ background.js
@@ -1,26 +1,9 @@
-let csrfToken;
-
-function getCsrfToken() {
-    // Get the CSRF token from the backend server
-    fetch('http://127.0.0.1:8000/csrf_token/', {
-        // Include cookies
-        credentials: 'include'
-    }).then(response => response.json())
-    .then(data => {
-        // Store the CSRF token inside the global variable
-        csrfToken = data.csrfToken;
-    }).catch(error => console.error('Error:', error));
-}
-
-// Get the CSRF token when the extension is loaded
-getCsrfToken();
-
 // Function to listen for messages from popup scripts
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     // Request to check an URL for phishing
     if (request.action === 'checkPhishing') {
         // Send the request to the server
-        checkUrl(request.url, request.isEnglish).then(response => {
+        checkUrl(request.url, request.isEnglish, request.csrfToken).then(response => {
             if (response && response.status === 'success') {
                 // Send the result back to the popup scripts
                 sendResponse({ result: response });
@@ -34,7 +17,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 });
 
 // asynchronous function to allow the JS engine perform other tasks while waiting for the response
-async function checkUrl(url, isEnglish) {
+async function checkUrl(url, isEnglish, csrfToken) {
     try {
         // Use await to pause the function execution until the network request completes
         const response = await fetch('http://127.0.0.1:8000/store_url/', {
@@ -44,6 +27,7 @@ async function checkUrl(url, isEnglish) {
                 'Content-Type': 'application/x-www-form-urlencoded',
                 'X-CSRFToken': csrfToken
             },
+            // Include cookies
             credentials: 'include',
             // Send the URL and the 'isEnglish' choice
             body: `url=${encodeURIComponent(url)}&isEnglish=${isEnglish}`
